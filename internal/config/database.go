6385912d4e239b94a756206bf9f8d87912499cb8
Kaique <kaiquesantiagop@gmail.com>
2025-05-06 14:46:19 -0300

    feat: db initializator logic and executable implemented

--- internal/config/database.go
+++ internal/config/database.go
@@ -18,40 +18,46 @@ func initializePostgres() (*gorm.DB, error) {
 		return nil, err
 	}
 
-	//migrate schema
+	logg.Debug("Postgres online.")
+
+	return db, nil
+}
+
+func MigrateSchemas() error {
+	dbUrl := "postgres://postgres:postgres@postgres:5432/datafisher_db?sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	if err != nil {
+		logg.Errorf("error initializing database: %v", err)
+		return err
+	}
+
 	if !db.Migrator().HasTable(&company.Company{}) {
 		err = db.AutoMigrate(&company.Company{})
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
 	if !db.Migrator().HasTable(&companyEmail.CompanyEmail{}) {
 		err = db.AutoMigrate(&companyEmail.CompanyEmail{})
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
 	if !db.Migrator().HasTable(&companyPhone.CompanyPhone{}) {
 		err = db.AutoMigrate(&companyPhone.CompanyPhone{})
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	if !db.Migrator().HasTable(&job.Job{}) {
-		err = db.AutoMigrate(&job.Job{})
+	if !db.Migrator().HasTable(&job.JobCnpj{}) {
+		err = db.AutoMigrate(&job.JobCnpj{})
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	if err != nil {
-		logg.Errorf("postgres automigration error: %v", err)
-		return nil, err
-	}
-	logg.Debug("Postgres online.")
-
-	return db, nil
+	return nil
 }
